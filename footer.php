de5a483812da29ebbf984cdb2f884a327bda39a8
reshmalr033 <42314384+reshmalr033@users.noreply.github.com>
2019-05-08 19:52:55 +0530

    Homepage design for bank

---
+++ footer.php
@@ -0,0 +1,74 @@
+
+<div class="footer-content">
+	 <div class="container">
+		 <div class="ftr-grids">
+			 
+                     
+                     
+                     
+                     
+                     
+			 <div class="col-md-3 ftr-grid">
+				 <h4>MAKING PAYMENTS</h4>
+				 <ul>
+					 <li><a href="#">> FUND TRANSFER</a></li>
+					 <li><a href="#">> PAY BILLS - ATM</a></li>
+					 <li><a href="#">> PAY BILLS - PHONE BANKING</a></li>
+					 <li><a href="#">> ONLINE TAX PAYMENT</a></li>
+					 <li><a href="#">> PAY BILLS - DEBIT CARD</a></li>
+					 <li><a href="#">> PAY CREDIT CARD BILLS</a></li>					 
+				 </ul>
+			 </div>
+			 <div class="col-md-3 ftr-grid">
+				 <h4>LOANS</h4>
+				 <ul>
+					 <li><a href="account.html">> GOLD LOANS</a></li>
+					 <li><a href="#">> HOUSING LOANS</a></li>
+					 <li><a href="#">> PROPERTY LOANS</a></li>
+					 <li><a href="#">> CAR LOANS</a></li>	
+                                         <li><a href="#">> PERSONAL LOANS</a></li>
+					 <li><a href="#">> EDUCATION AND CAREER LOANS</a></li>
+				 </ul>
+			 </div>
+			 <div class="col-md-3 ftr-grid">
+				 <h4>APPLY NOW</h4>
+				 <ul>
+					 <li><a href="#">> CREDIT CARD</a></li>
+					 <li><a href="#">> PERSONAL LOAN</a></li>
+					 <li><a href="#">> HOME LOAN</a></li>
+                                         <li><a href="#">> LOAN AGAINST MUTUAL FUNDS</a></li>
+					 <li><a href="#">> LOAN AGAINST PROPERTY</a></li>
+					 <li><a href="#">> SAVINGS ACCOUNT</a></li>
+					 				 
+				 </ul>
+			 </div>
+			 <div class="clearfix"></div>
+		 </div>
+	 </div>
+</div>
+<div class="footer">
+	 <div class="container">
+		 <div class="store">
+			 <ul>
+				 
+                             
+                             
+                             
+                             
+                             
+                             
+                             
+                        
+			 </ul>
+		 </div>		 
+		 <div class="copywrite">
+			 <p></p>
+		 </div>			 
+		 </div>
+	 </div>
+<!---->
+</div>
+<!---->
+</form>
+</body>
+</html>
