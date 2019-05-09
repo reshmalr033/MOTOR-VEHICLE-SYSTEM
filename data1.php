668fc91e835aa2b1ade3693ca98604c608d8df29
reshmalr033 <42314384+reshmalr033@users.noreply.github.com>
2019-05-09 21:58:31 +0530

    Toll survey

---
+++ data1.php
@@ -0,0 +1,159 @@
+<?php
+include 'menu_admin.php';
+?>
+<?php
+
+if($z=$_SESSION['username'])
+{
+    
+}
+ else {
+    header("location:index.php");    
+}
+?>
+
+				<!-- start-search-->
+			<div class="clearfix"> </div>
+        </div>
+
+		   <!--banner-->
+			 	<div class="banner two">
+                   <h2 class="second"></h2>
+			   </div>
+		</div>
+		</div>
+   <!--//banner-->
+   <!--/typography-->
+      <div class="typography">
+          <div class="container">
+              
+              
+              
+              
+              <script>
+window.onload = function () {
+	
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	
+	title:{
+		text:"Toll survey"
+	},
+	axisX:{
+		interval: 1
+	},
+	axisY2:{
+		interlacedColor: "rgba(1,77,101,.2)",
+		gridColor: "rgba(1,77,101,.1)",
+		title: "Number of Vehicles"
+	},
+	data: [{
+		type: "bar",
+		name: "companies",
+		axisYType: "secondary",
+		color: "#014D65",
+		dataPoints: [
+                    <?php
+$sel_des=mysql_query("select * from staff_reg where type='staff'");
+while($r=mysql_fetch_row($sel_des))
+{
+   $sel=mysql_query("select * from add_toll where toll_id='$r[12]'"); 
+   $r1=  mysql_num_rows($sel);
+    ?>
+			{ y: <?php echo $r1 ?>, label: "<?php echo $r[1] ?>" },
+                                <?php
+}
+?>
+			
+		]
+	}]
+});
+chart.render();
+
+}
+</script>
+<div id="chartContainer" style="height: 500px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+          </div>
+	  </div>
+	 <!--//typography-->
+
+		<!--footer-->
+			<div class="footer">
+				<div class="container">
+					 <div class="footer-inner">
+					    <div class="col-md-3 footer-grid">
+					       <h3 class="widget-title">Locate Us</h3>
+						   <p> <i class="glyphicon glyphicon-map-marker"></i> PO Box Collins Street<br>
+							Fort Cochin 683101<br>
+							Ernakulam</span></p>
+							<p class="phone"><i class="glyphicon glyphicon-earphone"></i> 0484 4869001</p>
+							<p><i class="glyphicon glyphicon-envelope"></i> <a href="mailto:info@example.com"> mail1@mvd.com</a></p>
+			              </div>
+					   <div class="col-md-3 footer-grid second">
+					      <h3 class="widget-title">Customer Services</h3>
+						      <ul class="menu-customer-service">
+						        <li><a href="#">Toll Free 1800-4000-3000 Delhi</a></li>
+								<li><a href="#">Contact Us</a></li>
+								<li><a href="#">Customer Care</a></li>
+								<li><a href="#">Policies</a></li>
+								<li><a href="#">Online Returns</a></li>
+								
+							  </ul>
+			           </div>
+					    <div class="col-md-3 footer-grid">
+					       <h3 class="widget-title">Customer Information</h3>
+						      <ul class="menu-customer-service">
+						        <li><a href="#">Toll Free 1800-4000-3000 Delhi</a></li>
+								<li><a href="#">Contact Us</a></li>
+								<li><a href="#">Customer Care</a></li>
+								<li><a href="#">Policies</a></li>
+								<li><a href="#">Online Returns</a></li>
+								
+							  </ul>
+			           </div>
+					  <div class="col-md-3 footer-grid lost">
+					       <h3 class="widget-title">Get in touch!</h3>
+						        <p>PO Box Collins Street<i class="glyphicon glyphicon-map-marker"></i> <br>
+								Fort Cochin 683101<br>
+								Ernakulam</span></p>
+								<p class="phone">0123-456-798 <i class="glyphicon glyphicon-earphone"></i></p>
+								<p><a href="mailto:info@example.com">mail@mvd33.com</a> <i class="glyphicon glyphicon-envelope"></i></p>
+			            </div>
+					   <div class="clearfix"></div>
+			         </div>
+			    </div>
+			</div>
+			<div class="copy">
+		              
+		            </div>
+			<!--start-smooth-scrolling-->
+			<script type="text/javascript">
+								jQuery(document).ready(function($) {
+									$(".scroll").click(function(event){		
+										event.preventDefault();
+										$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+									});
+								});
+								</script>
+							<!--start-smoth-scrolling-->
+						<script type="text/javascript">
+									$(document).ready(function() {
+										/*
+										var defaults = {
+								  			containerID: 'toTop', // fading element id
+											containerHoverID: 'toTopHover', // fading element hover id
+											scrollSpeed: 1200,
+											easingType: 'linear' 
+								 		};
+										*/
+										
+										$().UItoTop({ easingType: 'easeOutQuart' });
+										
+									});
+								</script>
+		<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+     </form>
+</body>
+</html>
