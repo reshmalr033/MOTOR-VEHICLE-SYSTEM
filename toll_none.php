f50795b090ef864547a0510fdf0f12ff11a33b74
reshmalr033 <42314384+reshmalr033@users.noreply.github.com>
2019-05-09 22:15:08 +0530

    No result found

---
+++ toll_none.php
@@ -0,0 +1,113 @@
+<?php
+include 'menu_admin.php';
+?>
+<?php
+
+$a=$_SESSION['username'];
+
+
+if(isset($_SESSION['username']))
+{
+    
+}
+ else {
+    header("location:index.php");    
+}
+?>
+				<!-- start-search-->
+			<div class="clearfix"> </div>
+        </div>
+
+		   <!--banner-->
+			 	<div class="">
+                   <center>
+                   <img src="images/no_results_illustration.jpg"class="img img-responsive">
+                   </center>
+			   </div>
+		</div>
+		</div>
+   <!--//banner-->
+   <!--/typography-->
+      
+	  </div>
+	 <!--//typography-->
+
+		<!--footer-->
+			<div class="footer">
+				<div class="container">
+					 <div class="footer-inner">
+					    <div class="col-md-3 footer-grid">
+					        <h3 class="widget-title">Locate Us</h3>
+						   <p> <i class="glyphicon glyphicon-map-marker"></i> PO Box Collins Street<br>
+							Fort Cochin 683101<br>
+							Ernakulam</span></p>
+							<p class="phone"><i class="glyphicon glyphicon-earphone"></i> 0484 4869001</p>
+							<p><i class="glyphicon glyphicon-envelope"></i> <a href="mailto:info@example.com"> mail1@mvd.com</a></p>
+			           </div>
+					   <div class="col-md-3 footer-grid second">
+					      <h3 class="widget-title">Customer Services</h3>
+						      <ul class="menu-customer-service">
+						        <li><a href="#">Toll Free 1800-4000-3000 Delhi</a></li>
+								<li><a href="#">Contact Us</a></li>
+								<li><a href="#">Customer Care</a></li>
+								<li><a href="#">Policies</a></li>
+								<li><a href="#">Online Returns</a></li>
+								
+							  </ul>
+			           </div>
+					    <div class="col-md-3 footer-grid">
+					       <h3 class="widget-title">Customer Information</h3>
+						      <ul class="menu-customer-service">
+						        <li><a href="#">Toll Free 1800-4000-3000 Delhi</a></li>
+								<li><a href="#">Contact Us</a></li>
+								<li><a href="#">Customer Care</a></li>
+								<li><a href="#">Policies</a></li>
+								<li><a href="#">Online Returns</a></li>
+								
+							  </ul>
+			           </div>
+					   <div class="col-md-3 footer-grid lost">
+					       <h3 class="widget-title">Get in touch!</h3>
+						        <p>PO Box Collins Street<i class="glyphicon glyphicon-map-marker"></i> <br>
+								Fort Cochin 683101<br>
+								Ernakulam</span></p>
+								<p class="phone">0123-456-798 <i class="glyphicon glyphicon-earphone"></i></p>
+								<p><a href="mailto:info@example.com">mail@mvd33.com</a> <i class="glyphicon glyphicon-envelope"></i></p>
+			            </div>
+					   <div class="clearfix"></div>
+			         </div>
+			    </div>
+			</div>
+			<div class="copy">
+		             
+		            </div>
+			<!--start-smooth-scrolling-->
+			<script type="text/javascript">
+								jQuery(document).ready(function($) {
+									$(".scroll").click(function(event){		
+										event.preventDefault();
+										$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+									});
+								});
+								</script>
+							<!--start-smoth-scrolling-->
+						<script type="text/javascript">
+									$(document).ready(function() {
+										/*
+										var defaults = {
+								  			containerID: 'toTop', // fading element id
+											containerHoverID: 'toTopHover', // fading element hover id
+											scrollSpeed: 1200,
+											easingType: 'linear' 
+								 		};
+										*/
+										
+										$().UItoTop({ easingType: 'easeOutQuart' });
+										
+									});
+								</script>
+		<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+     </form>
+</body>
+</html>
