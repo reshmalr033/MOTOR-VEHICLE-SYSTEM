abdca5f28d83805401aeb7bb9a6020223368f93e
reshmalr033 <42314384+reshmalr033@users.noreply.github.com>
2019-05-10 21:05:26 +0530

    update police_none

--- police_none.php
+++ police_none.php
@@ -36,49 +36,49 @@ if(isset($_SESSION['username']))
 				<div class="container">
 					 <div class="footer-inner">
 					    <div class="col-md-3 footer-grid">
-					       <h3 class="widget-title">Locate Us</h3>
-						   <p> <i class="glyphicon glyphicon-map-marker"></i> PO Box 15142 Collins Street<br>
-							120 King Street, London<br>
-							Victoria 2000 London</span></p>
-							<p class="phone"><i class="glyphicon glyphicon-earphone"></i> +61 0 1234 0000</p>
-							<p><i class="glyphicon glyphicon-envelope"></i> <a href="mailto:info@example.com"> mail@example.com</a></p>
+					        <h3 class="widget-title">Locate Us</h3>
+						   <p> <i class="glyphicon glyphicon-map-marker"></i> PO Box Collins Street<br>
+							Fort Cochin 683101<br>
+							Ernakulam</span></p>
+							<p class="phone"><i class="glyphicon glyphicon-earphone"></i> 0484 4869001</p>
+							<p><i class="glyphicon glyphicon-envelope"></i> <a href="mailto:info@example.com"> mail1@mvd.com</a></p>
 			           </div>
 					   <div class="col-md-3 footer-grid second">
-					       <h3 class="widget-title">Customer Services</h3>
+					      <h3 class="widget-title">Customer Services</h3>
 						      <ul class="menu-customer-service">
-						        <li><a href="#">Toll Free 1-800-carzone</a></li>
+						        <li><a href="#">Toll Free 1800-4000-3000 Delhi</a></li>
 								<li><a href="#">Contact Us</a></li>
 								<li><a href="#">Customer Care</a></li>
 								<li><a href="#">Policies</a></li>
 								<li><a href="#">Online Returns</a></li>
-								<li><a href="#">My Appointments</a></li>
+								
 							  </ul>
 			           </div>
 					    <div class="col-md-3 footer-grid">
 					       <h3 class="widget-title">Customer Information</h3>
 						      <ul class="menu-customer-service">
-						        <li><a href="#">Toll Free 1-800-carzone</a></li>
+						        <li><a href="#">Toll Free 1800-4000-3000 Delhi</a></li>
 								<li><a href="#">Contact Us</a></li>
 								<li><a href="#">Customer Care</a></li>
 								<li><a href="#">Policies</a></li>
 								<li><a href="#">Online Returns</a></li>
-								<li><a href="#">My Appointments</a></li>
+								
 							  </ul>
 			           </div>
 					   <div class="col-md-3 footer-grid lost">
 					       <h3 class="widget-title">Get in touch!</h3>
-						        <p>PO Box 15142 Collins Street<i class="glyphicon glyphicon-map-marker"></i> <br>
-								Victoria 2007 London<br>
-								Anona Headquarters</span></p>
-								<p class="phone">(+00) 0123-456-798 <i class="glyphicon glyphicon-earphone"></i></p>
-								<p><a href="mailto:info@example.com">mail@example.com</a> <i class="glyphicon glyphicon-envelope"></i></p>
-			           </div>
+						        <p>PO Box Collins Street<i class="glyphicon glyphicon-map-marker"></i> <br>
+								Fort Cochin 683101<br>
+								Ernakulam</span></p>
+								<p class="phone">0123-456-798 <i class="glyphicon glyphicon-earphone"></i></p>
+								<p><a href="mailto:info@example.com">mail@mvd33.com</a> <i class="glyphicon glyphicon-envelope"></i></p>
+			            </div>
 					   <div class="clearfix"></div>
 			         </div>
 			    </div>
 			</div>
 			<div class="copy">
-		              <p>© 2016 Roadster. All Rights Reserved | Design by <a href="http://w3layouts.com/">W3layouts</a> </p>
+		             
 		            </div>
 			<!--start-smooth-scrolling-->
 			<script type="text/javascript">
